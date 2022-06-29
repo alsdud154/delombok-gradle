3c0f7594b2a0bf2cf36071ec5d4c42f24f1a6682
velnova <alsdud154@blocko.io>
2022-06-29 15:24:18 +0900

    commit

--- src/main/java/delombok/dto/Member.java
+++ src/main/java/delombok/dto/Member.java
@@ -1,12 +1,14 @@
 package delombok.dto;
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Member {
     // 이름
+    @NonNull
     private String name;
     // 나이
     private Integer age;
